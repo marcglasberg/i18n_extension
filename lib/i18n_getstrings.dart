ae80829883feb29d0e339ddafdadcbae028dc833
Iurie Gutu <>
2021-03-30 17:31:35 +0300

    add plurals to .POT

--- lib/i18n_getstrings.dart
+++ lib/i18n_getstrings.dart
@@ -2,24 +2,63 @@ import 'dart:io';
 
 import 'package:analyzer/dart/analysis/utilities.dart';
 import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/token.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
+import 'package:equatable/equatable.dart';
+
+enum I18nRequiredModifiers { plural }
+
+class I18nSuffixes {
+  static const String i18n = 'i18n';
+  static const String fill = 'fill';
+  static const String plural = 'plural';
+  static const String version = 'version';
+  static const String allVersions = 'allVersions';
+
+  static const List<String> allSuffixes = const [
+    i18n,
+    fill,
+    plural,
+    version,
+    allVersions,
+  ];
+}
+
+class ExtractedString extends Equatable {
+  final String string;
+  final bool pluralRequired;
+
+  ExtractedString(this.string, {this.pluralRequired = false});
+
+  @override
+  List<Object> get props => [string, pluralRequired];
+}
+
+class DecodedSintax {
+  DecodedSintax._({
+    required this.valid,
+    this.modifiers = const [],
+  });
+
+  DecodedSintax.valid(List<I18nRequiredModifiers> modifiers)
+      : this._(valid: true, modifiers: modifiers);
+
+  DecodedSintax.invalid() : this._(valid: false);
+
+  final bool valid;
+  final List<I18nRequiredModifiers> modifiers;
+}
 
 class GetI18nStrings {
-  final List<String> suffixes;
   final String? sourceDir;
 
-  GetI18nStrings(this.sourceDir,
-      {this.suffixes = const [
-        "i18n",
-        "fill",
-        "plural",
-        "version",
-        "allVersions"
-      ]});
+  GetI18nStrings(
+    this.sourceDir,
+  );
 
-  List<String> run() {
+  List<ExtractedString> run() {
     var libDir = Directory(sourceDir!);
-    List<String> sourceStrings = [];
+    List<ExtractedString> sourceStrings = [];
     for (var f in libDir.listSync(recursive: true)) {
       if (f is File && f.path.endsWith(".dart")) {
         sourceStrings += processFile(f);
@@ -28,22 +67,21 @@ class GetI18nStrings {
     return sourceStrings;
   }
 
-  List<String> processFile(File f) {
+  List<ExtractedString> processFile(File f) {
     return processString(f.readAsStringSync());
   }
 
-  List<String> processString(String s) {
+  List<ExtractedString> processString(String s) {
     CompilationUnit unit =
         parseString(content: s, throwIfDiagnostics: false).unit;
-    var extractor = StringExtractor(suffixes);
+    var extractor = StringExtractor(I18nSuffixes.allSuffixes);
     unit.visitChildren(extractor);
     return extractor.strings;
   }
 }
 
-
 class StringExtractor extends UnifyingAstVisitor<void> {
-  List<String> strings = [];
+  List<ExtractedString> strings = [];
   List<String> suffixes;
 
   StringExtractor(this.suffixes);
@@ -55,8 +93,12 @@ class StringExtractor extends UnifyingAstVisitor<void> {
 
   @override
   void visitSimpleStringLiteral(SimpleStringLiteral node) {
-    if (_hasI18nSyntax(node, node.parent!)) {
-      strings.add(node.value);
+    final DecodedSintax sintax = _hasI18nSyntax(node, node.parent!);
+    if (sintax.valid) {
+      final ExtractedString s = ExtractedString(node.value,
+          pluralRequired:
+              sintax.modifiers.contains(I18nRequiredModifiers.plural));
+      strings.add(s);
     }
 
     return super.visitSimpleStringLiteral(node);
@@ -64,8 +106,13 @@ class StringExtractor extends UnifyingAstVisitor<void> {
 
   @override
   void visitAdjacentStrings(AdjacentStrings node) {
-    if (_hasI18nSyntax(node.strings.last, node.parent!)) {
-      strings.add(node.stringValue!);
+    final DecodedSintax sintax =
+        _hasI18nSyntax(node.strings.last, node.parent!);
+    if (sintax.valid) {
+      final ExtractedString s = ExtractedString(node.stringValue!,
+          pluralRequired:
+              sintax.modifiers.contains(I18nRequiredModifiers.plural));
+      strings.add(s);
     }
 
     // Dont' call the super method here, since we don't want to visit the
@@ -76,16 +123,20 @@ class StringExtractor extends UnifyingAstVisitor<void> {
   Check if the next sibling in the AST is a DOT operator
   and after that comes a literal included in our suffixes.
    */
-  bool _hasI18nSyntax(AstNode self, AstNode parent) {
-    var here = parent.beginToken;
-    while (here != parent.endToken) {
+  DecodedSintax _hasI18nSyntax(AstNode self, AstNode parent) {
+    Token? here = parent.beginToken;
+    while (here != null && here != parent.endToken) {
       if (here == self.beginToken &&
           here.next!.type.lexeme == "." &&
           suffixes.contains(here.next!.next!.value())) {
-        return true;
+        List<I18nRequiredModifiers> modifiers = List.empty(growable: true);
+        if (here.next!.next!.value() == I18nSuffixes.plural) {
+          modifiers.add(I18nRequiredModifiers.plural);
+        }
+        return DecodedSintax.valid(modifiers);
       }
-      here = here.next!;
+      here = here.next;
     }
-    return false;
+    return DecodedSintax.invalid();
   }
 }
